adf9b424e53fe5dd974f85e5aa86ddf2ab587a22
riqthedev <riqthedev@gmail.com>
2021-09-30 22:24:05 -0400

    added new endpoint

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ console.log("Hello World!")
 
 const express = require('express')
 
-
 const server = express()
-
 server.use(express.json())
 
 const port = process.env.PORT || 3000
+
+server.get('/' , (req,res) => {
+    res.json({message: "Happy Path!"})
+})
 
 server.get('/hello', (req,res) => {
     res.send('<h1> Hello There! </h1>')
