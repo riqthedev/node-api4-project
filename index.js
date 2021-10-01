c6088b6103b7df66d89b08ee2529365613b6145b
riqthedev <riqthedev@gmail.com>
2021-09-30 22:00:04 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+ require('dotenv').config()
+console.log("Hello World!")
+
+const express = require('express')
+
+
+const server = express()
+
+server.use(express.json())
+
+const port = process.env.PORT || 3000
+
+server.get('/hello', (req,res) => {
+    res.send('<h1> Hello There! </h1>')
+})
+
+server.listen(port, () =>  {
+console.log(`listening on ${port}`)
+})
+
